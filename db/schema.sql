3fae19c244534c1b68d994e03862dcb454b8300e
Mediogre <caseyarrington13@gmail.com>
2021-05-02 20:50:44 -0500

    initial setup

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+
+
+CREATE TABLE departments ();
+
+CREATE TABLE roles (
+    role_id INTEGER AUTO_INCREMENT PRIMARY KEY
+    title VARCHAR(30) NOT NULL;
+    salary INTEGER NOT NULL;
+    -- department TEXT MAKE FOREIGN KEY
+
+
+);
+
+CREATE TABLE employees ();
